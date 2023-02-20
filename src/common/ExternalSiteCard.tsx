6d2bf876f6725bdad45c4f615b454e3ab88d404a
pTheamtun <ptheamtu@gmail.com>
2023-02-20 14:39:49 +0900

    initial commit

---
+++ src/common/ExternalSiteCard.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+interface ExternalSiteCardProps {
+    siteName: string;
+    siteUrl: string;
+    siteIcon: string;
+    siteIconDescription: string;
+}
+
+const  ExternalSiteCard = (props: ExternalSiteCardProps) => {
+    return (
+        
+        <div> 
+        <h3>{props.siteName}</h3>
+        <a 
+        href={props.siteUrl}
+        target="_blank"
+        rel='noopener noreferer'
+        >
+        <Image
+        src={props.siteIcon}
+        alt={props.siteIconDescription||props.siteName}
+        width={20}
+        height={20}
+        priority
+        />
+        </a>
+      </div>);
+};
+
+export default ExternalSiteCard
